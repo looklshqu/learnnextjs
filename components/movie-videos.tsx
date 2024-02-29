e45d1ea36dc5ff13bd9b0bb63c821af2745ca89f
looklsh <looklsh38@gmail.com>
2024-02-29 15:54:40 +0900

    prefetch

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,8 +2,8 @@ import { API_URL } from "../app/constant";
 import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // console.log(`Fetching videos: ${Date.now()}`);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
   // throw new Error("Something new Error");
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
